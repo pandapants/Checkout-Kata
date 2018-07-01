865ccf4999d517fa1cccafabc531f6e9b06e5464
Amanda <amanda.tjernlund@gmail.com>
2018-07-01 11:26:56 +0100

    [Amanda] - Refactor to return total price based on SKU input

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -4,17 +4,21 @@ namespace CheckoutKata.Tests
 {
     internal class Checkout
     {
+        int totalPrice;
+
         public Checkout()
         {
+            totalPrice = 0;
         }
 
         internal void Scan(string sku)
         {
+            if (sku == "A") totalPrice += 50;
         }
 
         internal object GetTotalPrice()
         {
-            return 50;
+            return totalPrice;
         }
     }
 }
