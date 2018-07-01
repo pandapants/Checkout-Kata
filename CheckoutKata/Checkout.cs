2e0a4733e4948c627b57b33f901c8b0ff0cc510f
Amanda <amanda.tjernlund@gmail.com>
2018-07-01 11:23:22 +0100

    [Amanda] - First passing test. Return 50 when scanning SKU of A.

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CheckoutKata.Tests
+{
+    internal class Checkout
+    {
+        public Checkout()
+        {
+        }
+
+        internal void Scan(string sku)
+        {
+        }
+
+        internal object GetTotalPrice()
+        {
+            return 50;
+        }
+    }
+}
--- CheckoutKata/Tests/CheckoutTests.cs
+++ CheckoutKata/Tests/CheckoutTests.cs
@@ -10,5 +10,18 @@ namespace CheckoutKata.Tests
     [TestFixture]
     public class CheckoutTests
     {
+        [Test]
+        public void ScanningSkuofAReturnsTotalPriceOf50()
+        {
+            //Arrange
+            var checkout = new Checkout();
+
+            //Act
+            checkout.Scan("A");
+            var result = checkout.GetTotalPrice();
+
+            //Assert
+            Assert.AreEqual(50, result);
+        }
     }
 }
